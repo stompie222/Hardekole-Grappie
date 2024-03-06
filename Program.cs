9ceff6f1644a61fe620ad2742f72e57eaa723678
Marco Mulder <90194640+stompie222@users.noreply.github.com>
2024-03-06 10:51:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles(); // Serve static files from the "wwwroot" directory
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapFallbackToFile("index.html"); // Serve "index.html" for any unmatched request
+});
+
+app.Run();
+
---
+++ bundle.js
@@ -0,0 +1,16 @@
+﻿const yesButton = document.getElementById('yes');
+const noButton = document.getElementById('no');
+const responseElement = document.getElementById('response');
+
+noButton.addEventListener('click', () => {
+	yesButton.style.transform = 'scale(1.1)';
+	setTimeout(() => {
+		yesButton.style.transform = 'scale(1)';
+	}, 100);
+});
+
+yesButton.addEventListener('click', () => {
+	responseElement.style.fontWeight = 'bold';
+	responseElement.style.fontSize = '1.2em';
+	responseElement.innerText = 'You are GAYYYYYYY!';
+});
